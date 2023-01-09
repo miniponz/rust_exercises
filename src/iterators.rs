240e050463fb3c10c8938a65cdc5d6e80cf07710
Caroline Ponzini <cara@Carolines-MacBook-Air.local>
2023-01-09 15:50:12 -0800

    end day 1

---
+++ src/iterators.rs
@@ -0,0 +1,23 @@
+fn main() {
+  let v: Vec<i8> = vec![10, 20, 30];
+  let mut iter = v.iter();
+
+  let v0: Option<&i8> = iter.next();
+  println!("v0: {v0:?}");
+}
+
+// fn main() {
+//   let v: Vec<String> = vec![String::from("foo"), String::from("bar")];
+//   let mut iter = v.into_iter();
+
+//   let v0: Option<String> = iter.next();
+//   println!("v0: {v0:?}");
+
+    // for word in &v {
+    //     println!("word: {word}");
+    // }
+
+    // for word in v {
+    //     println!("word: {word}");
+    // }
+// }
